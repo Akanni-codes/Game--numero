8b8f2e3737c535082aa21c8befcecb7585d23276
Akanni <akanni029@gmail.com>
2025-02-05 22:27:17 -0300

    verificando chute e contando tentativas

--- app.js
+++ app.js
@@ -1,9 +1,34 @@
-let titulo = document.querySelector('h1');
-titulo.innerHTML = 'Game do numero secreto';
+let numeroSecreto = gerarNumeroAleatorio();
+let tentativas = 1;
 
-let paragrafo = document.querySelector('p')
-paragrafo.innerHTML = 'escolha um numero entre 1 e 10 !!';
+function exibirTextoNaTela(tag, texto) {
+    let campo = document.querySelector(tag);
+    campo.innerHTML = texto;
+}
+
+exibirTextoNaTela ('h1', 'Jogo do numero secreto');
+exibirTextoNaTela ('p', 'Escolha um numero de 1 a 10');
+
 
 function verificarChute() {
-    console.log ('o botão foi clicado')
+    let chute = document.querySelector('input').value;
+   
+    if (chute == numeroSecreto) {
+        exibirTextoNaTela('h1', 'Acertou!');
+        let palavraTentaviva = tentativas > 1 ? 'tentativas' : tentativas;
+        let mensagemTentativas = `Você descobriu o numero secreto com ${tentativas} ${palavraTentaviva}!`;
+        exibirTextoNaTela('p', mensagemTentativas)
+     } else {
+            if (chute > numeroSecreto) {
+                exibirTextoNaTela('p', 'O numero secreto é menor');
+            } else {
+                exibirTextoNaTela('p', 'O numero é maior');
+            }
+            tentativas++;
+        }
+    
 }
+
+function gerarNumeroAleatorio() {
+    return parseInt(Math.random() * 10 + 1)
+};
